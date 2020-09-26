c6a47a46ed6726d91fdbb7e77ba57137f2d7bfa5
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-09-26 22:01:08 +0600

    Interstitial add added

--- app/src/main/java/com/frskynet/femalebodylanguagesecrets/C1_DistanceActivity.java
+++ app/src/main/java/com/frskynet/femalebodylanguagesecrets/C1_DistanceActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -15,12 +17,19 @@ import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class C1_DistanceActivity extends Activity {
 
     private TextView distanceDesc;
     private ProgressDialog loading;
     private JSONParser jsonParser;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,14 @@ public class C1_DistanceActivity extends Activity {
         jsonParser = new JSONParser();
 
         loadFromSheet();
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/8691691433"); // Interstitial
+        AdRequest request = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mInterstitialAd.loadAd(request);
+        adEvent();
     }
 
     public void goToHomeBtnClick(View view) {
@@ -47,6 +64,7 @@ public class C1_DistanceActivity extends Activity {
                     public void onResponse(String response) {
                         distanceDesc.setText(jsonParser.parseItems(response, "Distance"));
                         loading.dismiss();
+                        loadAc();
                     }
                 },
                 new Response.ErrorListener() {
@@ -61,5 +79,60 @@ public class C1_DistanceActivity extends Activity {
         stringRequest.setRetryPolicy(policy);
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(stringRequest);
+    }
+
+    private void loadAc() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                interBtnClick();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 30000);
+    }
+
+    public void interBtnClick() {
+        if(mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+            Toast.makeText(this, "The interstitial wasn't loaded yet.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void adEvent() {
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Toast.makeText(getApplicationContext(), "Ad is loaded", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Toast.makeText(getApplicationContext(), "Ad failed to load", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdOpened() {
+                Toast.makeText(getApplicationContext(), "Ad is opened", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdClicked() {
+                Toast.makeText(getApplicationContext(), "Ad is clicked", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                Toast.makeText(getApplicationContext(), "Ad left application", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdClosed() {
+                Toast.makeText(getApplicationContext(), "Ad is closed", Toast.LENGTH_LONG).show();
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
     }
 }
